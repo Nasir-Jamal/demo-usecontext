de530b10d7a172ea3096d5d8ccb36d3fbc601812
Nasir-Jamal <39132449+Nasir-Jamal@users.noreply.github.com>
2021-04-17 22:51:37 +0800

    update master

--- src/App.js
+++ src/App.js
@@ -2,21 +2,33 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Parent from './Parent';
-
+import ValueContext from './ValueContext';
 function App() {
   let [number, setNumber] = useState(45); // want to pass number=45 to Child component
                                          // but App.js don't have Child component, It has Parent component
                                          // and Parent component has Child
                                          // so We pass num={number} in Parent here 
                                          // and Parent will pass num={propos.num} to Child
-                                         
+  
+  let value = 78;                                       
   return (
-    <div >
-      Hello WOrld 
-      <Parent num={number}></Parent>
-      <button onClick={()=>{setNumber(++number)}}>Update Number</button>
-      <button onClick={()=>{setNumber(--number)}}>Decrease Number</button>
-    </div>
+
+    <ValueContext.Provider value={value}>
+     <div>
+       Hello WOrld!
+       <Parent></Parent>
+     </div>
+    </ValueContext.Provider>
+    
+    /*
+   <div >
+     Hello WOrld 
+     <Parent num={number}></Parent>
+     <button onClick={()=>{setNumber(++number)}}>Update Number</button>
+     <button onClick={()=>{setNumber(--number)}}>Decrease Number</button>
+   </div>
+    */
+    
   );
 }
 
