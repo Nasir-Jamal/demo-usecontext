4272074dab9af46710a083125398dde85c3bfa95
Nasir-Jamal <39132449+Nasir-Jamal@users.noreply.github.com>
2021-04-17 22:10:42 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Parent from './Parent';
 
 function App() {
+  let [number, setNumber] = useState(45); // want to pass number=45 to Child component
+                                         // but App.js don't have Child component, It has Parent component
+                                         // and Parent component has Child
+                                         // so We pass num={number} in Parent here 
+                                         // and Parent will pass num={propos.num} to Child
+                                         
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      Hello WOrld 
+      <Parent num={number}></Parent>
+      <button onClick={()=>{setNumber(++number)}}>Update Number</button>
+      <button onClick={()=>{setNumber(--number)}}>Decrease Number</button>
     </div>
   );
 }
